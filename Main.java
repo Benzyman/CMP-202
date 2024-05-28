f5217fa0a38b60441baea63af5f67903ed455599
Benzyman <129904335+Benzyman@users.noreply.github.com>
2024-05-28 15:25:51 +0100

    Add files via upload
    The palindrome code

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
-public static void main(String[] args) {
-    Person person = new Person();
-    person.setName("prince");
-    System.out.println(person.getName());
-
-    person.setPassword("3642864");
-    System.out.println(person.getPassword());
-
-    Physics physics = new Physics();
-    System.out.println(physics.initialVelocity1(23, 2);
-    System.out.println(physics.calculateVelocity(23, 53, 2));
+public class main{
+    public static void main(){
+        String word = "Hannah";
+        char[] wordList = word.toCharArray();
+        for (i = wordList.length -1, i >= 0, i--){
+            String word2 += wordList[i]; 
+        }
+        if (word == word2){
+            System.out.println("The word is the same backword");
+        }
+        else{
+            System.out.println("The word is not the same backword");
+        }
+    }
 }
